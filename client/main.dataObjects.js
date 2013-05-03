2c32b35c3bb694eb9d2505d7010d970fa9e78516
stephan hantigk <s.hantigk@runlevel3.de>
2013-05-04 01:41:43 +0200

    first running version

--- client/main.dataObjects.js
+++ client/main.dataObjects.js
@@ -1,8 +1,15 @@
 
-var getDataObjects= getCachedData('dataObjects', 1000000);
-Template.dataObjects.dataObjects= function() {
-    console.log(getDataObjects.toString());
-    var help= getDataObjects();
-    console.log("help", help);
-    return help;
+Template.dataObjects.schemas= function() {
+    return getCachedData('getTypeVersions', 10000);
 }
+
+Template.dataObjects.rendered= function() {
+    $('div.objectTypes').jstree({
+        // the `plugins` array allows you to configure the active plugins on this instance
+//        "plugins" : ["themes","html_data","ui"],
+        // each plugin you have included can have its own config object
+//        "core" : { "initially_open" : [ "phtml_1" ] }
+        // it makes sense to configure a plugin only if overriding the defaults
+    });
+}
+
---
+++ client/mainMenu.js
@@ -0,0 +1,53 @@
+
+var menuItems= {};
+var menuTabs= [];
+
+var addMenuItem= function(id, title, content) {
+    id= 'menuItemId-' + id;
+    var item= {
+        id: id,
+        title: title,
+        content: content,
+    };
+    menuItems[id]= item;
+    menuTabs.push(item);
+}
+
+addMenuItem("dataObjects", "DataObjects", function() {
+    return new Handlebars.SafeString(Template.dataObjects());
+});
+
+addMenuItem("tab2", "Tab 2", "tab 2");
+
+Template.main.menu= function() {
+    return menuTabs;
+}
+
+Template.main.events({
+    'click ul.nav-tabs a': function(event) {
+        var a= event.currentTarget;
+        var href= $(a).attr("href").replace(/^[\.\#]/, '');
+        Session.set('mainMenuTab', href);
+        return true;
+    },
+});
+
+var selectedTab= function() {
+    return Session.get('mainMenuTab');
+}
+
+Template.main.active= function(tabName) {
+    return selectedTab() === tabName ? 'active' : '';
+}
+
+Template.main.enabled= function(tabName) {
+    return selectedTab() === tabName;
+}
+
+Template.main.helper=function(tabName) {
+    var item= menuItems[selectedTab()];
+    if (!item) return;
+
+    return typeof item.content === 'function' ? item.content() : item.content;
+};
+
