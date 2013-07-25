6ca0ff1bbd87a6b9bb940ce413825b2e3ad309c1
Stephan Hantigk <develop@runlevel3.de>
2013-07-25 17:57:05 +0200

    some mor changes for modelRun

--- client/main.modelRun.js
+++ client/main.modelRun.js
@@ -15,7 +15,10 @@ var getModelArgs= function() {
     return _getModelArgs(modelId);
 };
 
+var getMatchingTypes= DataObjectTools.getCachedData('getMatchingTypes');
 var getMatchingObjects= DataObjectTools.getCachedData('getMatchingObjects');
+
+var injectVar= DataObjectTools.injectVar;
 
 Template.modelRunTitle.title= function() {
     var model= getModel();
@@ -55,54 +58,96 @@ Template.modelRunBody.isRef= function() {
     return typeof this.type === 'object';
 };
 
-Template.modelRunBody.typeInput= function() {
-    var templateName;
-    switch ( this.type ) {
-        case 'Double': templateName= 'Double'; break;
-        case 'Location': templateName= 'Location'; break;
-        case 'Date': templateName='Date'; break;
-        default: 
-            templateName= 'Model'; break;
-    }
-
-    if ( !templateName ) return;
-
-    return new Handlebars.SafeString(Template['valueInput' + templateName](this));
-};
-
 var cleanType= function( type ) {
     var result= _.clone(type);
     if ( 'info' in result ) delete result.info;
     return result;
 };
 
-Template.valueInputModel.matching= function() {
-    var result= getMatchingObjects(cleanType(this.type));
+var buildContextForModel= function( context ) {
+    var result= getMatchingTypes(cleanType(context.type));
+
     var typeNames= [];
 
-    var value= {
-        type: undefined,
-        value: undefined,
-    }
+    if ( !result ) return { typeName: typeNames };
 
-    if ( result.models && result.models.length ) typeNames.push({ label: 'Model', type: 'model', value: value, });
+    result.requestedType= context.type;
+
+    var setFn= function( typeName ) {
+        injectVar(result, 'value')({
+            label: typeName.label,
+            type: typeName.type,
+        });
+    };
+
+    if ( result.models && result.models.length ) typeNames.push({ label: 'Model', type: 'model', setType: setFn, });
     if ( result.schemas && result.schemas.length ) {
-        typeNames.push({ label: 'AgroObjects', type: 'schemas', value: value, });
-        typeNames.push({ label: 'AgroObject Map', type: 'schemas|Map', value: value, });
-        typeNames.push({ label: 'Nearest AgroObject', type: 'schemas|Nearest', value: value, });
+        typeNames.push({ label: 'AgroObject Map', type: 'map', setType: setFn, });
+        typeNames.push({ label: 'Nearest AgroObject', type: 'nearest', setType: setFn, });
+        typeNames.push({ label: 'AgroObjects', type: 'agroObj', setType: setFn, });
     }
 
-    value.type= typeNames[0];
 
     result.typeName= typeNames;
-    result.value= value;
     return result;
 };
 
+
+Template.valueInput.input= function() {
+    var templateName;
+    var context= this;
+    switch ( this.type ) {
+        case 'Double': templateName= 'Double'; break;
+        case 'Location': templateName= 'Location'; break;
+        case 'Date': templateName='Date'; break;
+        default: 
+            templateName= 'Model';
+            context= buildContextForModel(this);
+            break;
+    }
+
+    if ( !templateName ) return;
+
+    return new Handlebars.SafeString(Template['valueInput' + templateName](context));
+};
+
+Template.valueInputModel.currentLabel= function() {
+    var value= injectVar(this, 'value', {
+        label: (this.typeName[0] || {}).label,
+        type:  (this.typeName[0] || {}).type,
+    })();
+    return value.label;
+}
+
 Template.valueInputModel.events({
     'click li[value]': function( event ) {
-console.log(this);
-        this.value.type= this;
-console.log(this);
+        this.setType(this);
     },
 });
+
+Template.valueInputModel.values= function() {
+
+    var value= injectVar(this, 'value')();
+    if ( !value ) return;
+
+    switch ( value.type ) {
+        case 'model':
+            return this.models.map(function( model ){
+                return { id: model._id.toHexString(), name: model.name };
+            });
+
+        case 'map':
+        case 'nearest':
+            return this.schemas.map(function( schema ){
+                return { id: schema.objectType + '::' + schema.version, name: schema.objectType + '/' + schema.version };
+            });
+
+        case 'agroObj':
+            var result= getMatchingObjects(cleanType(this.requestedType));
+            if ( !result || !result.agroObjs ) return;
+
+            return result.agroObjs.map(function( agroObj ) {
+                return { id: agroObj._id.toHexString(), name: agroObj.name };
+            })
+    }
+};
