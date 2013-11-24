dc35324d2cb679d166831c6eba81766272b872c4
stephan hantigk <rl3.github@steppicrew.de>
2013-11-24 13:43:43 +0100

    removed old files

--- .old/client/main.editValue.js
+++
@@ -1,8 +0,0 @@
-
-Template.editValue.events({
-    'click a': function( event ) {
-        DataObjectTools.injectGlobalVar('valueInput')(this);
-        DataObjectTools.showModal($('#valueInput'));
-        return false;
-    },
-});
