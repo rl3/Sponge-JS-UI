f3b8edd3479af332566112b2079a69404cecec2d
stephan hantigk <rl3.github@steppicrew.de>
2015-03-27 21:31:15 +0100

    some more comments

--- client/modal.js
+++ client/modal.js
@@ -3,20 +3,25 @@
 var modalStackDepth= 0;
 
 var showModal= function( $dialog, cb ) {
+
+    // get all currently open modal dialogs
     var $currentModals= $('.modal.in');
 
     var currentStackDepth= ++modalStackDepth;
 
+    // set dialog's state to not-open for hidden-event handlers
     var onHide= function( ev ) {
         if ( this !== ev.target || modalStackDepth !== currentStackDepth ) return;
+
+        $dialog.off('hide', onHide);
 
         $dialog.data('dialog-is-open', false);
     };
 
+    // if we are the top-most dialog, show previous hidden dialogs
     var onHidden= function( ev ) {
         if ( this !== ev.target || modalStackDepth !== currentStackDepth ) return;
 
-        $dialog.off('hide', onHide);
         $dialog.off('hidden', onHidden);
 
         modalStackDepth--;
@@ -24,15 +29,18 @@ var showModal= function( $dialog, cb ) {
         $currentModals.modal('show');
     };
 
+    // mark dialog as open and call callback
     $dialog.one('shown', function() {
         $dialog.data('dialog-is-open', true);
         if ( cb ) return cb();
     });
+
     $dialog.on('hide', onHide);
     $dialog.on('hidden', onHidden);
 
     if ( !$currentModals.length ) return $dialog.modal('show');
 
+    // hide currently open dialogs and show this one
     $currentModals.one('hidden', function() {
         $dialog.modal('show');
     }).modal('hide');
